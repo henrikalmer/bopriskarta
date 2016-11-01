b7eb5008ab5fdaa9b66be338b03c9ea0a99cb1eb
Henrik Almér <henrik.almer@booli.se>
2016-11-01 14:45:18 +0100

    Add data and pre-processing script

---
+++ preprocess.R
@@ -0,0 +1,60 @@
+library(dplyr)
+library(magrittr)
+
+
+setwd("/Users/Henrik/Repositories/bopriskarta/")
+
+
+normalize.sold.df <- function(df) {
+	##  Set column names
+	colnames(df) <- c("indexAdjustedSoldPrice", "soldPrice", "soldDate", "livingArea",
+		"additionalArea", "plotArea", "rooms", "floor", "rent", "rentPerSQM", "operatingCost",
+		"operatingCostPerSQM", "objectType", "latitude", "longitude")
+
+	##  Replace "None" with NA
+	df[df == "None"] <- NA
+	
+	##  Cast columns
+	cast.cols <- which(colnames(df) %in% c("livingArea", "additionalArea", "plotArea", "rooms",
+		"floor", "rent", "rentPerSQM", "operatingCost", "operatingCostPerSQM"))
+	df[, cast.cols] %<>% lapply(function(x) as.numeric(x))
+	df$soldDate <- as.POSIXct(df$soldDate)
+	df$objectType <- as.factor(df$objectType)
+
+	##  Filter "bad" datapoints
+	df <- df[!is.na(df$soldPrice), ]
+	df <- df[!is.na(df$livingArea), ]
+	df <- df[df$livingArea > 1, ]
+	df <- df[df$livingArea < 400, ]
+	df <- df[df$indexAdjustedSoldPrice != df$soldPrice, ]
+
+	##  Calculate sqm prices
+	df$soldSQMPrice <- df$soldPrice / df$livingArea
+	df$indexAdjustedSQMPrice <- df$indexAdjustedSoldPrice / df$livingArea
+	
+	return(df)
+}
+
+
+##  Load data from CSV
+sold.2012 <- read.csv('data/csv/index_adjusted_objects_2012.csv', stringsAsFactors=FALSE)
+sold.2013 <- read.csv('data/csv/index_adjusted_objects_2013.csv', stringsAsFactors=FALSE)
+sold.2014 <- read.csv('data/csv/index_adjusted_objects_2014.csv', stringsAsFactors=FALSE)
+sold.2015 <- read.csv('data/csv/index_adjusted_objects_2015.csv', stringsAsFactors=FALSE)
+sold.2016 <- read.csv('data/csv/index_adjusted_objects_2016.csv', stringsAsFactors=FALSE)
+
+##  Normalize data
+sold.2012 <- normalize.sold.df(sold.2012)
+sold.2013 <- normalize.sold.df(sold.2013)
+sold.2014 <- normalize.sold.df(sold.2014)
+sold.2015 <- normalize.sold.df(sold.2015)
+sold.2016 <- normalize.sold.df(sold.2016)
+
+##  Append dataframes and write to disk
+sold <- rbind(sold.2012, sold.2013, sold.2014, sold.2015, sold.2016)
+save(sold, 'data/Rdata/sold.Rdata')
+
+## Aggregate on median values per location (point)
+sold$objectType <- as.numeric(sold$objectType)
+sold.agg <- sold %>% group_by(longitude, latitude) %>% summarise_each(funs(median))
+save(sold.agg, 'data/Rdata/sold-aggregate.Rdata')
